ed1f84e73dd5467acfeefe2b05ed1e867eb36f61
T Gibson <taylornicolegibson@gmail.com>
2022-09-18 13:52:40 -0400

    Update End.cs

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class End : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
