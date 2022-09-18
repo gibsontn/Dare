e332553b43e1c1734a84302120c59b088b8dfb23
T Gibson <taylornicolegibson@gmail.com>
2022-09-18 13:47:56 -0400

    scenes

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+
+}
